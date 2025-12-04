ca344ee905cc60b4b44df259080b5909c0844f05
say-no-to-sleep <genezhao63@proton.me>
2025-12-03 20:45:32 -0500

    Initial Commit

---
+++ VacuumClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VacuumClone
+//
+//  Created by Eugene Zhao on 2025-12-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VacuumClone/VacuumCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacuumCloneApp.swift
+//  VacuumClone
+//
+//  Created by Eugene Zhao on 2025-12-03.
+//
+
+import SwiftUI
+
+@main
+struct VacuumCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
